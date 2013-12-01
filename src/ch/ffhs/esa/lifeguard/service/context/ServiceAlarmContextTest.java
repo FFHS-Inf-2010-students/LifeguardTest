e7a60460a8a4e2416e3579c35a25d804cffa99c3
David Daniel <fidel@daniels.li>
2013-12-01 13:57:09 +0100

    Move to api level 10, use android test case for context involving tests

--- src/ch/ffhs/esa/lifeguard/service/context/ServiceAlarmContextTest.java
+++ src/ch/ffhs/esa/lifeguard/service/context/ServiceAlarmContextTest.java
@@ -1,46 +1,82 @@
 package ch.ffhs.esa.lifeguard.service.context;
 
-import junit.framework.TestCase;
+import ch.ffhs.esa.lifeguard.alarm.context.AlarmContext;
+import ch.ffhs.esa.lifeguard.alarm.context.ServiceAlarmContext;
+import ch.ffhs.esa.lifeguard.alarm.state.AlarmState;
+import ch.ffhs.esa.lifeguard.alarm.state.AlarmStateId;
+import ch.ffhs.esa.lifeguard.alarm.state.AlarmStateListener;
+import ch.ffhs.esa.lifeguard.alarm.state.InitialState;
+import android.test.AndroidTestCase;
 
 /**
- * @author 
+ * @author
  *
  */
-public class ServiceAlarmContextTest extends TestCase {
+public class ServiceAlarmContextTest extends AndroidTestCase {
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
+    private ServiceAlarmContext alarmContext;
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
+    private static class StateListener implements AlarmStateListener {
+        public boolean wasCalled = false;
 
-	/**
-	 * Test method for {@link ch.ffhs.esa.lifeguard.service.context.ServiceAlarmContext#setNext(ch.ffhs.esa.lifeguard.service.state.AlarmState)}.
-	 */
-	public final void testSetNext() {
-		fail("Not yet implemented");
-	}
+        public void onStateChanged (AlarmContext context)
+        { wasCalled = true; }
+    };
 
-	/**
-	 * Test method for {@link ch.ffhs.esa.lifeguard.service.context.ServiceAlarmContext#getState()}.
-	 */
-	public final void testGetState() {
-		fail("Not yet implemented");
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp () throws Exception
+    {
+        super.setUp ();
+        alarmContext = new ServiceAlarmContext (getContext ());
+    }
 
-	/**
-	 * Test method for {@link ch.ffhs.esa.lifeguard.service.context.ServiceAlarmContext#addListener(ch.ffhs.esa.lifeguard.service.state.AlarmStateListener)}.
-	 */
-	public final void testAddListener() {
-		fail("Not yet implemented");
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown () throws Exception
+    {
+        super.tearDown ();
+        alarmContext.cancel ();
+    }
 
+    /**
+     * Test method for
+     * {@link ch.ffhs.esa.lifeguard.service.context.ServiceAlarmContext#setNext(ch.ffhs.esa.lifeguard.service.state.AlarmState)}
+     * .
+     */
+    public final void testSetNext ()
+    {
+        alarmContext.setNext (new InitialState ());
+        assertEquals (alarmContext.getState ().getId (), AlarmStateId.INIT);
+    }
+
+    /**
+     * Test method for
+     * {@link ch.ffhs.esa.lifeguard.service.context.ServiceAlarmContext#getState()}
+     * .
+     */
+    public final void testGetState ()
+    {
+        AlarmState state = alarmContext.getState ();
+        assertNotNull (state);
+    }
+
+    /**
+     * Test method for
+     * {@link ch.ffhs.esa.lifeguard.service.context.ServiceAlarmContext#addListener(ch.ffhs.esa.lifeguard.service.state.AlarmStateListener)}
+     * .
+     */
+    public final void testAddListener ()
+    {
+        StateListener listener = new StateListener();
+        alarmContext.addListener (listener);
+        alarmContext.setNext (new InitialState ());
+        assertTrue (listener.wasCalled);
+    }
 }
