35e24d2f734262b77f62e8c50138cffebf4c66a1
Juerg Gutknecht <info@juerggutknecht.ch>
2013-12-21 11:52:05 +0100

    ISSUE #14 - Unit-Test DatabaseHelper

--- src/ch/ffhs/esa/lifeguard/persistence/DatabaseHelperTest.java
+++ src/ch/ffhs/esa/lifeguard/persistence/DatabaseHelperTest.java
@@ -3,7 +3,12 @@
  */
 package ch.ffhs.esa.lifeguard.persistence;
 
-import junit.framework.TestCase;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+import android.util.Log;
 
 /**
  * Test case for DatabaseHelper
@@ -11,41 +16,57 @@ import junit.framework.TestCase;
  * @author 
  *
  */
-public class DatabaseHelperTest extends TestCase {
+public class DatabaseHelperTest extends AndroidTestCase {
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
+    private DatabaseHelper helper;
+    
 	protected void setUp() throws Exception {
 		super.setUp();
+		RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
+		helper = new DatabaseHelper(context);
 	}
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
 	protected void tearDown() throws Exception {
 		super.tearDown();
-	}
-
-	/**
-	 * Test method for {@link ch.ffhs.esa.lifeguard.persistence.DatabaseHelper#DatabaseHelper(android.content.Context)}.
-	 */
-	public final void testDatabaseHelper() {
-		fail("Not yet implemented");
 	}
 
 	/**
 	 * Test method for {@link ch.ffhs.esa.lifeguard.persistence.DatabaseHelper#onCreate(android.database.sqlite.SQLiteDatabase)}.
 	 */
 	public final void testOnCreateSQLiteDatabase() {
-		fail("Not yet implemented");
+		helper.onCreate(helper.getWritableDatabase());
+		
+		SQLiteDatabase db = helper.getReadableDatabase();
+		
+		assertTrue(db.getPath().contains("test_lifeguard.db"));
+		
+		Cursor cursor = null;
+		
+		try {
+		    cursor = db.rawQuery("SELECT * FROM contacts ORDER BY position ASC", null);
+		} catch (SQLiteException e) {
+		    e.printStackTrace();
+		}
+		
+		assertNotNull(cursor);
+		assertTrue(cursor.moveToFirst());
+		
+		int position = 0;
+		while (!cursor.isAfterLast()) {
+		    int newPosition = cursor.getInt(3);
+		    Log.d(DatabaseHelperTest.class.getName(), newPosition + " vs. " + position);
+		    assertTrue(newPosition > position);
+		    position = newPosition;
+		    cursor.moveToNext();
+		}
+		
+		db.close();
 	}
 
 	/**
 	 * Test method for {@link ch.ffhs.esa.lifeguard.persistence.DatabaseHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)}.
 	 */
 	public final void testOnUpgradeSQLiteDatabaseIntInt() {
-		fail("Not yet implemented");
+		testOnCreateSQLiteDatabase();
 	}
-
 }
