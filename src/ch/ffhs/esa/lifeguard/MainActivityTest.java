703764f28325d7a45bf591e37daf38bac0e7715c
David Daniel <fidel@daniels.li>
2013-12-22 11:57:21 +0100

    Fix layout test
    Sorry for the reformatting of the code, it was filled with tabs.

--- src/ch/ffhs/esa/lifeguard/MainActivityTest.java
+++ src/ch/ffhs/esa/lifeguard/MainActivityTest.java
@@ -1,5 +1,6 @@
 package ch.ffhs.esa.lifeguard;
 
+import ch.ffhs.esa.lifeguard.ui.ClockTickFormatter;
 import android.content.Intent;
 import android.test.ActivityUnitTestCase;
 import android.widget.Button;
@@ -8,108 +9,132 @@ import android.widget.ToggleButton;
 
 /**
  * @author Juerg Gutknecht <juerg.gutknecht@students.ffhs.ch>
- *
  */
-public class MainActivityTest
-	extends ActivityUnitTestCase<MainActivity> {
+public class MainActivityTest extends ActivityUnitTestCase<MainActivity>
+{
 
     private MainActivity activity;
-    
-	public MainActivityTest() {
-		super(MainActivity.class);
-	}
 
-	/* (non-Javadoc)
-	 * @see android.test.ActivityUnitTestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
-		super.setUp();
-		Intent intent = new Intent(
-	        getInstrumentation().getTargetContext(), MainActivity.class
-        );
-		startActivity(intent, null, null);
-		activity = getActivity();
-	}
+    public MainActivityTest ()
+    {
+        super (MainActivity.class);
+    }
 
-	/* (non-Javadoc)
-	 * @see android.test.ActivityUnitTestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
-	
-	public final void testLayout() {
-	    // check if delay label exists
-	    int delayLabelId = ch.ffhs.esa.lifeguard.R.id.textViewDelayLabel;
-	    assertNotNull(activity.findViewById(delayLabelId));
-	    TextView delayLabel = (TextView)activity.findViewById(delayLabelId);
-	    assertEquals(
-            activity.getString(ch.ffhs.esa.lifeguard.R.string.main_label_delay),
-            delayLabel.getText()
-        );
-	    
-	    // check if delay view exists
-	    int delayViewId = ch.ffhs.esa.lifeguard.R.id.textViewDelay;
-	    assertNotNull(activity.findViewById(delayViewId));
-	    TextView delayView = (TextView)activity.findViewById(delayViewId);
-	    assertEquals(
-	        activity.getString(ch.ffhs.esa.lifeguard.R.string.main_value_delay),
-	        delayView.getText()
-        );
-	    
-	    // check if progress bar exists
-	    int progressBarId = ch.ffhs.esa.lifeguard.R.id.progressBarDelay;
-	    assertNotNull(activity.findViewById(progressBarId));
-	    
-	    // check if alarm switch label exists
-	    int alarmSwitchLabelId = ch.ffhs.esa.lifeguard.R.id.textViewAlarmSwitch;
-	    assertNotNull(activity.findViewById(alarmSwitchLabelId));
-	    TextView alarmSwitchLabel = (TextView)activity.findViewById(alarmSwitchLabelId);
-        assertEquals(
-            activity.getString(ch.ffhs.esa.lifeguard.R.string.main_label_alarm_switch),
-            alarmSwitchLabel.getText()
-        );
-	    
-	    // check if alarm switch exists
-	    int alarmSwitchId = ch.ffhs.esa.lifeguard.R.id.toggleButtonAlarmSwitch;
-	    assertNotNull(activity.findViewById(alarmSwitchId));
-	    ToggleButton alarmSwitch = (ToggleButton)activity.findViewById(alarmSwitchId);
-	    assertTrue(alarmSwitch.isEnabled());
-	    assertTrue(!alarmSwitch.isChecked());
-	    
-	    // check if SOS button exists
-	    int sosButtonId = ch.ffhs.esa.lifeguard.R.id.SOSButton;
-	    assertNotNull(activity.findViewById(sosButtonId));
-	    Button sosButton = (Button)activity.findViewById(sosButtonId);
-	    assertEquals(
-            activity.getString(ch.ffhs.esa.lifeguard.R.string.main_label_sos),
-            sosButton.getText()
-        );
-	    assertTrue(sosButton.isEnabled());
-	    
-	    // check if SOS text exists
-	    int sosTextId = ch.ffhs.esa.lifeguard.R.id.textViewSOSButton;
-	    assertNotNull(activity.findViewById(sosTextId));
-	    //TextView sosText = (TextView)activity.findViewById(sosTextId);
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see android.test.ActivityUnitTestCase#setUp()
+     */
+    protected void setUp () throws Exception
+    {
+        super.setUp ();
+        Intent intent = new Intent (getInstrumentation ().getTargetContext (),
+                MainActivity.class);
+        startActivity (intent, null, null);
+        activity = getActivity ();
+    }
 
-	/**
-	 * Test method for {@link ch.ffhs.esa.lifeguard.MainActivity#openConfiguration()}.
-	 */
-	public final void testOpenConfiguration() {
-		activity.openConfiguration();
-		Intent triggeredIntent = getStartedActivityIntent();
-		assertNotNull("Intent was null", triggeredIntent);
-		assertEquals(ConfigurationActivity.class.getName(), triggeredIntent.getComponent().getClassName());
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see android.test.ActivityUnitTestCase#tearDown()
+     */
+    protected void tearDown () throws Exception
+    {
+        super.tearDown ();
+    }
 
-	/**
-	 * Test method for {@link ch.ffhs.esa.lifeguard.MainActivity#viewContacts()}.
-	 */
-	public final void testViewContacts() {
-		activity.viewContacts();
-		Intent triggeredIntent = getStartedActivityIntent();
-        assertNotNull("Intent was null", triggeredIntent);
-        assertEquals(ContactListActivity.class.getName(), triggeredIntent.getComponent().getClassName());
-	}
+    public final void testLayout ()
+    {
+        // check if delay label exists
+        int delayLabelId = ch.ffhs.esa.lifeguard.R.id.textViewDelayLabel;
+        assertNotNull (activity.findViewById (delayLabelId));
+        TextView delayLabel = (TextView) activity.findViewById (delayLabelId);
+        assertEquals (
+                activity.getString (ch.ffhs.esa.lifeguard.R.string.main_label_delay),
+                delayLabel.getText ());
+
+        // check if delay view exists
+        int delayViewId = ch.ffhs.esa.lifeguard.R.id.textViewDelay;
+        assertNotNull (activity.findViewById (delayViewId));
+
+        // We cannot really test this, since the layout itself is not aware of
+        // the correct value - the layout gets initialized with the default
+        // value that gets overriden by the services real value (that gets
+        // fetched from the configuration and set by the value according the
+        // configuration or the default value. We would need to make sure the
+        // service has already replied with its status message and the initial
+        // view has already setup the screen).
+//        String tickValue = activity
+//                .getSharedPreferences (Lifeguard.APPLICATION_SETTINGS, 0)
+//                .getString (
+//                        "alarmDelay",
+//                        activity.getString (ch.ffhs.esa.lifeguard.R.string.main_value_delay));
+//
+//        long maxTick = Long.parseLong (tickValue);
+//
+//        TextView delayView = (TextView) activity.findViewById (delayViewId);
+//        assertEquals (ClockTickFormatter.format (0L, maxTick), delayView.getText ());
+
+        // check if progress bar exists
+        int progressBarId = ch.ffhs.esa.lifeguard.R.id.progressBarDelay;
+        assertNotNull (activity.findViewById (progressBarId));
+
+        // check if alarm switch label exists
+        int alarmSwitchLabelId = ch.ffhs.esa.lifeguard.R.id.textViewAlarmSwitch;
+        assertNotNull (activity.findViewById (alarmSwitchLabelId));
+        TextView alarmSwitchLabel = (TextView) activity
+                .findViewById (alarmSwitchLabelId);
+        assertEquals (
+                activity.getString (ch.ffhs.esa.lifeguard.R.string.main_label_alarm_switch),
+                alarmSwitchLabel.getText ());
+
+        // check if alarm switch exists
+        int alarmSwitchId = ch.ffhs.esa.lifeguard.R.id.toggleButtonAlarmSwitch;
+        assertNotNull (activity.findViewById (alarmSwitchId));
+        ToggleButton alarmSwitch = (ToggleButton) activity
+                .findViewById (alarmSwitchId);
+        assertTrue (alarmSwitch.isEnabled ());
+        assertTrue (!alarmSwitch.isChecked ());
+
+        // check if SOS button exists
+        int sosButtonId = ch.ffhs.esa.lifeguard.R.id.SOSButton;
+        assertNotNull (activity.findViewById (sosButtonId));
+        Button sosButton = (Button) activity.findViewById (sosButtonId);
+        assertEquals (
+                activity.getString (ch.ffhs.esa.lifeguard.R.string.main_label_sos),
+                sosButton.getText ());
+        assertTrue (sosButton.isEnabled ());
+
+        // check if SOS text exists
+        int sosTextId = ch.ffhs.esa.lifeguard.R.id.textViewSOSButton;
+        assertNotNull (activity.findViewById (sosTextId));
+        // TextView sosText = (TextView)activity.findViewById(sosTextId);
+    }
+
+    /**
+     * Test method for
+     * {@link ch.ffhs.esa.lifeguard.MainActivity#openConfiguration()}.
+     */
+    public final void testOpenConfiguration ()
+    {
+        activity.openConfiguration ();
+        Intent triggeredIntent = getStartedActivityIntent ();
+        assertNotNull ("Intent was null", triggeredIntent);
+        assertEquals (ConfigurationActivity.class.getName (), triggeredIntent
+                .getComponent ().getClassName ());
+    }
+
+    /**
+     * Test method for {@link ch.ffhs.esa.lifeguard.MainActivity#viewContacts()}
+     * .
+     */
+    public final void testViewContacts ()
+    {
+        activity.viewContacts ();
+        Intent triggeredIntent = getStartedActivityIntent ();
+        assertNotNull ("Intent was null", triggeredIntent);
+        assertEquals (ContactListActivity.class.getName (), triggeredIntent
+                .getComponent ().getClassName ());
+    }
 }
